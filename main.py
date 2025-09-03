490f78a5f3abd462eb5ed10026f0161d6568a634
johns9aj-git <johns9aj@mail.uc.edu>
2025-09-03 04:34:08 -0400

    book_word_counter bad

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("greetings boots")
+# python
+
+def get_book_text(filepath): # Stores all characters in memory.
+    with open(filepath) as f:
+        return f.read()
+    
+def book_word_counter(filepath):  
+    text = get_book_text(filepath)  # Use the parameter, not hardcoded path
+    words = text.split() 
+    word_count = len(words)
+    return word_count
+
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    word_count = book_word_counter("books/frankenstein.txt")
+    print(text)
+    print(word_count)
+
+
+main()
