01e01fc1d9f34224fafd6cb35bc1577dc8d8c3c0
ajohns2020 <andrew2001johns@gmail.com>
2025-09-03 05:52:16 -0400

    main.py Encoding changes

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # python
 
 def get_book_text(filepath): # Stores all characters in memory.
-    with open(filepath) as f:
+    with open(filepath, "r", encoding="utf-8", errors="ignore") as f: # Tries to ignore Window's Windows-1252 encoding, might not need it. 
         return f.read()
+
     
 def book_word_counter(filepath):  
     text = get_book_text(filepath)  # Use the parameter, not hardcoded path
     words = text.split() 
-    word_count = len(words) + " words found in the document" 
+    word_count = f"{len(words)} words found in the document." 
     return word_count
 
 
