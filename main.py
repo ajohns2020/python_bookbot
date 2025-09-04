477063dfedbb46a562f94ca90ebbe63d09238926
ajohns2020 <andrew2001johns@gmail.com>
2025-09-03 23:24:30 -0400

    main.py | sys.argv | CREATION

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 # python
+# Imports 
+import os 
+import sys
+
 # Importing functions from stats.py for better organization
 from stats import get_book_text, book_word_counter, book_char_frequencies, book_final_report
 
+# Checking if at least two arguments are provided (script name and filepath)
+sys.argv[0] = "main.py" # Script name
+filepath = sys.argv[1] # Filepath argument
+
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+
+
+
+
+
+
+
     
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    word_count = book_word_counter("books/frankenstein.txt")
-    char_count = book_char_frequencies("books/frankenstein.txt")
-    report = book_final_report("books/frankenstein.txt")
+    print ("Reading book from :" , filepath)
+    text = get_book_text()
+    word_count = book_word_counter()
+    char_count = book_char_frequencies()
+    report = book_final_report()
     print(text)
     print(word_count)
     print(char_count)
