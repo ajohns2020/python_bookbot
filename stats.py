57738dfb008f32d649593a0113859c5813b43433
ajohns2020 <andrew2001johns@gmail.com>
2025-09-03 21:46:24 -0400

    stats.py | book_word_counter | MODIFIED

--- stats.py
+++ stats.py
@@ -11,7 +11,7 @@ def get_book_text(filepath): # Stores all characters in memory.
 def book_word_counter(filepath):  
     text = get_book_text(filepath)  # Use the parameter, not hardcoded path
     words = text.split() 
-    word_count = f"{len(words)} words found in the document." 
+    word_count = f"Found {len(words)} total words" 
     return word_count
 
 def book_char_frequencies(filepath):  
